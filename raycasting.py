5ca712b8fa2ce78e0746872469b7084b6483dc9e
Lakshya123-cyber <lakshya.raikwal@giis.edu.sg>
2022-10-24 15:34:10 +0800

    3D Projection

--- raycasting.py
+++ raycasting.py
@@ -57,13 +57,32 @@ class RayCasting:
             else:
                 depth = depth_hor
 
-            # Draw for Debugging
+            # remove fishbowl effect
+            depth *= math.cos(self.game.player.angle - ray_angle)
+
+            """
+            Draw for Debugging
             pg.draw.line(
                 self.game.screen,
                 "yellow",
                 (100 * ox, 100*oy),
                 (100 * ox + 100 * depth * cos_a, 100 * oy + 100 * depth * sin_a),
                 2,
+            )
+            """
+
+            # Projection
+            proj_height = SCREEN_DIST / (depth + 0.0001)
+
+            # Draw walls
+            color = [255 / (1 + depth * 5 * 0.00002)] * 3
+            pg.draw.rect(
+                self.game.screen,
+                color,
+                (ray * SCALE,
+                HALF_HEIGHT - proj_height // 2,
+                SCALE,
+                proj_height)
             )
 
             ray_angle += DELTA_ANGLE
