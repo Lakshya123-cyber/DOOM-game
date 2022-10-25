60d9e6286d7e2fdd195783f5ab027e4695e751a0
Lakshya123-cyber <lakshya.raikwal@giis.edu.sg>
2022-10-25 14:14:04 +0800

    Weapon

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from raycasting import *
 from object_renderer import *
 from sprite_object import *
 from object_handler import *
+from weapon import *
+from sound import *
 
 class Game:
     def __init__(self):
@@ -23,11 +25,14 @@ class Game:
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
         self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f} DOOM")
@@ -37,6 +42,7 @@ class Game:
 
         # 3D PROJECTION
         self.object_renderer.draw()
+        self.weapon.draw()
 
         # 2D PROJECTION
         # self.map.draw()
@@ -47,6 +53,8 @@ class Game:
             if e.type == pg.QUIT or (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            self.player.single_fire_event(e)
+            
 
     def run(self):
         while True:
--- sprite_object.py
+++ sprite_object.py
@@ -3,8 +3,16 @@ from settings import *
 import os
 from collections import deque
 
+
 class SpriteObject:
-    def __init__(self, game, path="resources/sprites/static_sprites/candlebra.png", pos=(10.5, 3.5), scale=0.7, shift=0.27):
+    def __init__(
+        self,
+        game,
+        path="resources/sprites/static_sprites/candlebra.png",
+        pos=(10.5, 3.5),
+        scale=0.7,
+        shift=0.27,
+    ):
         self.game = game
         self.player = game.player
         self.x, self.y = pos
@@ -12,28 +20,37 @@ class SpriteObject:
         self.IMAGE_WIDTH = self.image.get_width()
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
         self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height()
-        self.dx, self.dy, self.theta, self.screen_x, self.dist, self.norm_dist = 0, 0, 0, 0, 1, 1
+        self.dx, self.dy, self.theta, self.screen_x, self.dist, self.norm_dist = (
+            0,
+            0,
+            0,
+            0,
+            1,
+            1,
+        )
         self.sprite_half_width = 0
         self.SPRITE_SCALE = scale
         self.SPRITE_HEIGHT_SHIFT = shift
 
     def get_sprite_projection(self):
         proj = SCREEN_DIST / self.norm_dist * self.SPRITE_SCALE
-        proj_width, proj_height =  proj * self.IMAGE_RATIO, proj
+        proj_width, proj_height = proj * self.IMAGE_RATIO, proj
 
         image = pg.transform.scale(self.image, (proj_width, proj_height))
 
         self.sprite_half_width = proj_width // 2
         height_shift = proj_height * self.SPRITE_HEIGHT_SHIFT
-        pos = self.screen_x - self.sprite_half_width, HALF_HEIGHT - proj_height // 2 + height_shift
+        pos = (
+            self.screen_x - self.sprite_half_width,
+            HALF_HEIGHT - proj_height // 2 + height_shift,
+        )
 
         self.game.raycasting.objects_to_render.append((self.norm_dist, image, pos))
 
     def get_sprite(self):
         dx = self.x - self.player.x
         dy = self.y - self.player.y
-        self.dx ,self.dy = dx, dy
-
+        self.dx, self.dy = dx, dy
         self.theta = math.atan2(dy, dx)
 
         delta = self.theta - self.player.angle
@@ -45,20 +62,30 @@ class SpriteObject:
 
         self.dist = math.hypot(dx, dy)
         self.norm_dist = self.dist * math.cos(delta)
-        if -self.IMAGE_HALF_WIDTH < self.screen_x < (WIDTH + self.IMAGE_HALF_WIDTH) and self.norm_dist > 0.5:
+        if (
+            -self.IMAGE_HALF_WIDTH < self.screen_x < (WIDTH + self.IMAGE_HALF_WIDTH)
+            and self.norm_dist > 0.5
+        ):
             self.get_sprite_projection()
 
     def update(self):
         self.get_sprite()
 
+
 class AnimatedSprite(SpriteObject):
-    def __init__(self, game, path="resources/sprites/animated_sprites/green_light/0.png", pos=(11.5, 3.5), scale=0.8, shift=0.15, animation_time=120):
+    def __init__(
+        self,
+        game,
+        path="resources/sprites/animated_sprites/green_light/0.png",
+        pos=(11.5, 3.5),
+        scale=0.8,
+        shift=0.16,
+        animation_time=120,
+    ):
         super().__init__(game, path, pos, scale, shift)
         self.animation_time = animation_time
-
-        self.path = path.rsplit('/', 1)[0]
+        self.path = path.rsplit("/", 1)[0]
         self.images = self.get_images(self.path)
-
         self.animation_time_prev = pg.time.get_ticks()
         self.animation_trigger = False
 
@@ -83,6 +110,6 @@ class AnimatedSprite(SpriteObject):
         images = deque()
         for file_name in os.listdir(path):
             if os.path.isfile(os.path.join(path, file_name)):
-                img = pg.image.load(path + '/' + file_name).convert_alpha()
+                img = pg.image.load(path + "/" + file_name).convert_alpha()
                 images.append(img)
         return images
